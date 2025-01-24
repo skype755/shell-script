6f75070a6e47dfdf34730c58868f3702384995c2
aws755 <skypeskype755@gmail.com>
2025-01-24 12:17:29 +1100

    shell-script

--- 02-sample.sh
+++ 02-sample.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-log_file="/path/to/logfile.log"
-max_size=1000000 # 1MB
-if [ $(wc -c < "$log_file") -gt $max_size ]; then
-    mv "$log_file" "$log_file.old"
-    touch "$log_file"
-fi
+LOG_FILE="/var/log/syslog"
+KEYWORDS="error|failed|warning"
+OUTPUT_FILE="/var/log/monitor.log"
