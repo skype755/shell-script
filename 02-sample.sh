80e0d6f2867ca0f74441b8b808c1cd9c5f698ed2
aws755 <skypeskype755@gmail.com>
2025-01-24 12:14:17 +1100

    shell-script

--- 02-sample.sh
+++ 02-sample.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-access_log="/var/log/apache/access.log"
-error_log="/var/log/apache/error.log"
-# Analyze access log
-echo "Top 10 IP addresses:"
-awk '{print $1}' "$access_log" | sort | uniq -c | sort -nr | head -n 10
-# Analyze error log
-echo "Errors by type:"
-awk '{print $9}' "$error_log" | sort | uniq -c | sort -nr
+log_file="/path/to/logfile.log"
+max_size=1000000 # 1MB
+if [ $(wc -c < "$log_file") -gt $max_size ]; then
+    mv "$log_file" "$log_file.old"
+    touch "$log_file"
+fi
